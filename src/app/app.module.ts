01b48e71f5c8639e390fe274bbf573ce9786b0cb
mardanga <gaitan@CESVIARG>
2018-04-30 17:15:47 -0300

    modelos

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { APP_ROUTING } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent
+  ],
+  imports: [
+    BrowserModule, APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
